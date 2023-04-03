68f13ac64dab1bafab8d731ea890357564a06d66
ArmandoCecilio <117837439+ArmandoCecilio@users.noreply.github.com>
2023-04-02 20:24:33 -0500

    Update Create_Account.php

--- Create_Account.php
+++ Create_Account.php
@@ -1,46 +1,28 @@
 <?php
-session_start();
-
-function createAccount($username, $password, $existingUsers) {
-    foreach ($existingUsers as $user) {
-        if ($user['username'] === $username) {
-            return false;
-        }
-    }
-
-    $newUser = array('username' => $username, 'password' => $password);
-    array_push($existingUsers, $newUser);
-
-    return true;
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "oleum";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
 
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+$username = $_POST["username"];
+$password = $_POST["password"];
 
-    // perform validation
-    if(empty($username) || empty($password)) {
-        header("Location: create-account.php?error=1");
-        exit();
-    }
-
-    // check if user already exists
-    $existingUsers = array(
-        array('username' => 'user1', 'password' => 'pass1'),
-        array('username' => 'user2', 'password' => 'pass2'),
-        array('username' => 'user3', 'password' => 'pass3')
-    );
-
-    $result = createAccount($username, $password, $existingUsers);
-
-    if($result){
-        // create session
-        $_SESSION['username'] = $username;
-        header("Location: ClientProfile.html");
-        exit();
-    } else {
-        header("Location: Create_Account.php?error=2");
-        exit();
-    }
+$sql = "SELECT * FROM login WHERE username='$username'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+  echo "Username already exists.";
+} else {
+  $sql = "INSERT INTO login (username, password) VALUES ('$username', '$password')";
+  if ($conn->query($sql) === TRUE) {
+    header("Location: ClientProfile.html");
+    exit();
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
 }
-?>
+
+$conn->close();
