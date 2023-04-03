a9d3a635f3a42a0dd0c3505f7a25b9ebc24f07a8
ArmandoCecilio <117837439+ArmandoCecilio@users.noreply.github.com>
2023-04-02 20:22:23 -0500

    Update ClientProfile.php

--- ClientProfile.php
+++ ClientProfile.php
@@ -1,5 +1,4 @@
 <?php
-// Connect to MySQL server
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -9,7 +8,6 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// Get form data
 $fullname = $_POST["fullname"];
 $Address1 = $_POST["Address1"];
 $Address2 = $_POST["Address2"];
@@ -17,15 +15,12 @@ $city = $_POST["City"];
 $state = $_POST["State"];
 $zip = $_POST["Zipcode"];
 
-// Insert data into the login table
 $sql = "INSERT INTO ClientProfile (fullname, address1, address2, city, state, zip) VALUES ('$fullname', '$Address1','$Address2', '$city', '$state', '$zip')";
 if ($conn->query($sql) === TRUE) {
-  // Redirect to profile management page
   header("Location: ClientProfile.html");
   exit();
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
 
-// Close database connection
 $conn->close();
