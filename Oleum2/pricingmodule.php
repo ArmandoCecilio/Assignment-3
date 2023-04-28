c430928380173304f78625d10fe4a933eaac8131
Fausto <116304800+devfamm@users.noreply.github.com>
2023-04-27 22:09:10 -0500

    Update pricingmodule.php

--- Oleum2/pricingmodule.php
+++ Oleum2/pricingmodule.php
@@ -1,5 +1,15 @@
 <?php 
-
-
-
-?>
+function getsugestedprice($conn, $userid, $userstate, $requestedgallons)
+{
+  $query="SELECT COUNT(*) FROM fuelquoteform WHERE user_id=$userid";
+  $count=$conn->query($query);
+  const CurrentPrice=1.50;
+  $LocationFactor = $userstate=='TX'?0.02 :0.04;
+  $RateHistoryFactor =  $count>0?0.01:0.00;
+  $GallonsRequestedFactor = $requestedgallons>1000? 0.02:0.03;
+  const CompanyProfitFactor = 0.1;
+  $Margin =  CurrentPrice * ($LocationFactor - $RateHistoryFactor + $GallonsRequestedFactor + CompanyProfitFactor);
+  
+  $SuggestedPrice = CurrentPrice + $Margin;
+  return $SuggestedPrice;
+}
