8ae3961f16a19d2ca6b7feafd4a5333aeaf7ba5c
Chase <115040405+ncmoreno@users.noreply.github.com>
2023-04-28 14:40:15 -0500

    Update pricingmodule.php

--- Oleum2/pricingmodule.php
+++ Oleum2/pricingmodule.php
@@ -1,17 +1,35 @@
-<?php 
-function getsugestedprice($conn, $userid, $userstate, $requestedgallons)
-{
-  $query="SELECT * FROM fuelquoteform WHERE member_ID='$userid'";
+function getSuggestedPrice($conn, $userId, $userState, $requestedGallons) {
+    // Validate the user input for requested gallons
+    if (!is_numeric($requestedGallons) || $requestedGallons <= 0) {
+        throw new Exception('Invalid requested gallons value');
+    }
 
-  $count=mysqli_num_rows($conn->query($query));
-  $CurrentPrice=1.50;
-  $LocationFactor = $userstate=='TX'?0.02 :0.04;
-  $RateHistoryFactor =  $count>0?0.01:0.00;
-  $GallonsRequestedFactor = $requestedgallons>1000? 0.02:0.03;
-  $CompanyProfitFactor = 0.1;
-  $Margin =  $CurrentPrice * ($LocationFactor - $RateHistoryFactor + $GallonsRequestedFactor + $CompanyProfitFactor);
+    // Use a more accurate and updated current price for fuel
+    $currentPrice = 1.75;
+
+    // Use prepared statements to prevent SQL injection attacks
+    $stmt = $conn->prepare("SELECT * FROM fuelquoteform WHERE member_ID = ?");
+    $stmt->bind_param("i", $userId);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    // Check for errors with the database query
+    if (!$result) {
+        throw new Exception('Error with database query');
+    }
+
+    // Get the number of rows in the result set
+    $count = $result->num_rows;
+
+    $locationFactor = $userState == 'TX' ? 0.02 : 0.04;
+    $rateHistoryFactor = $count > 0 ? 0.01 : 0.00;
+    $gallonsRequestedFactor = $requestedGallons > 1000 ? 0.02 : 0.03;
+    $companyProfitFactor = 0.1;
+    $margin = $currentPrice * ($locationFactor - $rateHistoryFactor + $gallonsRequestedFactor + $companyProfitFactor);
   
-  $SuggestedPrice = $CurrentPrice + $Margin;
-  return $SuggestedPrice;
+    $suggestedPrice = $currentPrice + $margin;
+
+    return $suggestedPrice;
 }
+
 
